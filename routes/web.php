8adfe21e1dc02b46030f05e5701a51a806237a3d
Yarosalav <bondarchuck.yaroslaw@gmail.com>
2022-12-07 14:02:35 +0200

    added dashboard and blog views; ui and jetstream packages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () {return view('welcome');});
+
+Route::get('/index', [App\Http\Controllers\Blog\BlogShowController::class, 'indexPage'])->name('index');
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/monitor', [App\Http\Controllers\Dashboard\MainController::class, 'indexPage'])->name('indexPage');
 });
+
+
+Auth::routes();
